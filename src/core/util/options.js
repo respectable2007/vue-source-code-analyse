8dddabb2d66378a26749d3915b864444832f3c89
respectable2007 <zhanghui8876@163.com>
2019-05-24 14:58:40 +0800

    options lifecycle

--- src/core/util/options.js
+++ src/core/util/options.js
@@ -170,6 +170,7 @@ strats.data = function (
 /**
  * Hooks and props are merged as arrays.
  */
+/* 各生命周期钩子选项合并策略，返回一个数组 */
 function mergeHook (
   parentVal: ?Array<Function>,
   childVal: ?Function | ?Array<Function>
@@ -186,6 +187,7 @@ function mergeHook (
     : res
 }
 
+/* 去重 */
 function dedupeHooks (hooks) {
   const res = []
   for (let i = 0; i < hooks.length; i++) {
